73a72ab09f5b8eb989635cbfc6f91ce22eeb0c17
steveoro <steve.alloro@gmail.com>
2021-03-15 10:58:28 +0100

    re-synch w/ base engine; v. 0.87

--- lib/version.rb
+++ lib/version.rb
@@ -3,7 +3,7 @@
 #
 # == Versioning codes
 #
-#   - version:  7.85
+#   - version:  7.87
 #   - author:   Steve A.
 #
 # Framework version number usually differs from core engine versioning number.
@@ -22,10 +22,10 @@ module Version
   MAJOR   = '7'
 
   # Minor version.
-  MINOR   = '85'
+  MINOR   = '87'
 
   # Current build version.
-  BUILD   = '20210310'
+  BUILD   = '20210315'
 
   # Full versioning for the current release.
   FULL    = "#{MAJOR}.#{MINOR}.#{BUILD} (#{CORE})"
