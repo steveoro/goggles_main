76ece46ea6d1651461f977036cd188f0a2c8e87b
steveoro <steve.alloro@gmail.com>
2021-05-01 17:31:33 +0200

    removed usage of subseeded application helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,5 @@
 #
 # Common parent helper
 module ApplicationHelper
-  # Returns a static html checkbox icon for a given boolean value
-  # == Params
-  # - bool_value: true or false|nil to change the icon
-  def check_mark_icon_for(bool_value)
-    content_tag(:i, '', class: bool_value ? 'fa fa-check-square-o' : 'fa fa-square-o')
-  end
+  # (unused for the moment)
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,9 +3,5 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
-  describe '#check_mark_icon_for' do
-    it 'renders two different values for true and false' do
-      expect(helper.check_mark_icon_for(true)).not_to eq(helper.check_mark_icon_for(false))
-    end
-  end
+  # (unused for the moment)
 end
