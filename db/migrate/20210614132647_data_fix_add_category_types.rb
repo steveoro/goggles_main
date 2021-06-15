232d8e8540aaf62087815aa9913e53fcaed26952
steveoro <steve.alloro@gmail.com>
2021-06-15 12:21:10 +0200

    even more dev bindings for yarn

--- db/migrate/20210614132647_data_fix_add_category_types.rb
+++ db/migrate/20210614132647_data_fix_add_category_types.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'goggles_db/version'
+
 class DataFixAddCategoryTypes < ActiveRecord::Migration[6.0]
   def self.up
     puts "\r\n--> Adding category types for new seasons..."
