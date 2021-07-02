8c9cb555e6e222ac0f6cba55fa7d4ec36f3e5c12
steveoro <steve.alloro@gmail.com>
2021-07-02 20:23:10 +0200

    MRR solver strategy

--- app/strategies/solver/meeting_relay_result.rb
+++ app/strategies/solver/meeting_relay_result.rb
@@ -14,6 +14,124 @@ module Solver
   # Resolves the request for building a new GogglesDb::MeetingRelayResult.
   #
   class MeetingRelayResult < BaseStrategy
-    # TODO
+    # Returns the first entity row found that matches at least one of the key attributes.
+    # Returns +nil+ when not found.
+    #
+    # == Finder criteria:
+    # 1. id: matched by equality
+    # 2. bindings match
+    #
+    def finder_strategy
+      id = value_from_req(key: 'meeting_relay_result_id', nested: 'meeting_relay_result', sub_key: 'id')
+      # Priority #1
+      return GogglesDb::MeetingRelayResult.find_by_id(id) if id.to_i.positive?
+
+      # Priority #2
+      solve_bindings
+      return nil unless required_bindings.values.all?(&:present?)
+
+      GogglesDb::MeetingRelayResult.where(required_bindings).first
+    end
+    #-- -----------------------------------------------------------------------
+    #++
+
+    # Returns a newly created target entity instance, serialized if and only if
+    # all the bindings were solved and the resulting row was valid.
+    #
+    # == Returns:
+    # - +nil+ until all required bindings are solved;
+    # - a new target entity instance when done, saved successfully if valid,
+    #   and yielding any validation erros as #error_messages.
+    def creator_strategy
+      solve_bindings
+      return nil unless required_bindings.values.all?(&:present?)
+
+      new_instance = GogglesDb::MeetingRelayResult.new
+      bindings.each { |key, solved| new_instance.send("#{key}=", solved) unless solved.nil? }
+      new_instance.save # Don't throw validation errors
+      new_instance
+    end
+    #-- -----------------------------------------------------------------------
+    #++
+
+    protected
+
+    # Hash of required bindings/associations that have to be resolved, using format:
+    #
+    #     key_column_name.to_sym => solver_instance || value_from_req
+    #
+    # A direct attribute binding will be resolved to +nil+ if can't be found inside the
+    # current data set after a call to #solve!.
+    #
+    # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/MethodLength
+    def init_bindings
+      @bindings = {
+        meeting_program_id: Solver::Factory.for('MeetingProgram', root_key?('meeting_program') ? req : req['meeting_relay_result']),
+        # Give priority to the nested version of Swimmer & Team:
+        team_id: Solver::Factory.for('Team', nested_key?('meeting_relay_result', 'team') ? req['meeting_relay_result'] : req),
+        team_affiliation_id: Solver::Factory.for(
+          'TeamAffiliation',
+          nested_key?('meeting_relay_result', 'team_affiliation') ? req['meeting_relay_result'] : req
+        ),
+        # Needed to discriminate between different relays from same team:
+        relay_code: value_from_req(
+          key: 'relay_code',
+          nested: 'meeting_relay_result', sub_key: 'relay_code'
+        ),
+
+        # Optional fields:
+        rank: value_from_req(
+          key: 'meeting_relay_result_rank',
+          nested: 'meeting_relay_result', sub_key: 'rank'
+        ) || 0,
+        reaction_time: value_from_req(
+          key: 'meeting_relay_result_reaction_time',
+          nested: 'meeting_relay_result', sub_key: 'reaction_time'
+        ) || 0.0,
+
+        minutes: value_from_req(
+          key: 'meeting_relay_result_minutes',
+          nested: 'meeting_relay_result', sub_key: 'minutes'
+        ) || 0,
+        seconds: value_from_req(
+          key: 'meeting_relay_result_seconds',
+          nested: 'meeting_relay_result', sub_key: 'seconds'
+        ) || 0,
+        hundredths: value_from_req(
+          key: 'meeting_relay_result_hundredths',
+          nested: 'meeting_relay_result', sub_key: 'hundredths'
+        ) || 0,
+
+        standard_points: value_from_req(
+          key: 'meeting_relay_result_standard_points',
+          nested: 'meeting_relay_result', sub_key: 'standard_points'
+        ) || 0,
+        meeting_points: value_from_req(
+          key: 'meeting_relay_result_meeting_points',
+          nested: 'meeting_relay_result', sub_key: 'meeting_points'
+        ) || 0,
+
+        entry_time_type_id: Solver::Factory.for(
+          'EntryTimeType',
+          root_key?('entry_time_type') ? req : req['meeting_relay_result']
+        ) || GogglesDb::EntryTimeType::LAST_RACE_ID,
+        disqualification_code_type_id: Solver::Factory.for(
+          'DisqualificationCodeType',
+          root_key?('disqualification_code_type') ? req : req['disqualification_code_type']
+        )
+      }
+    end
+    # rubocop:enable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/MethodLength
+
+    private
+
+    # Filtered hash of minimum required field bindings
+    def required_bindings
+      @bindings.select do |key, _value|
+        %i[
+          meeting_program_id team_affiliation_id team_id relay_code
+        ].include?(key)
+      end
+    end
   end
 end
--- spec/strategies/solver/meeting_relay_result_spec.rb
+++ spec/strategies/solver/meeting_relay_result_spec.rb
@@ -3,5 +3,201 @@
 require 'rails_helper'
 
 RSpec.describe Solver::MeetingRelayResult, type: :strategy do
-  # TODO
+  context 'before #solve!,' do
+    it_behaves_like(
+      'Solver strategy, bindings, finder & creator, before #solve!',
+      'MeetingRelayResult', Solver::MeetingRelayResult
+    )
+  end
+  #-- -------------------------------------------------------------------------
+  #++
+
+  #
+  # INVALID data: BAD ID, @ root
+  #
+  context 'with INVALID #req data (non-existing id @ root lv.),' do
+    let(:fixture_req) { { 'meeting_relay_result_id' => -1 } }
+    it_behaves_like('Solver strategy, bindings, UNSOLVABLE req, after #solve!', 'MeetingRelayResult')
+  end
+
+  #
+  # INVALID data: BAD ID, @ sub-entity
+  #
+  context 'with INVALID #req data (non-existing id @ sub-entity lv.),' do
+    let(:fixture_req) { { 'meeting_relay_result' => { 'id' => -1 } } }
+    it_behaves_like('Solver strategy, bindings, UNSOLVABLE req, after #solve!', 'MeetingRelayResult')
+  end
+  #-- -------------------------------------------------------------------------
+  #++
+
+  [
+    ->(row) { { 'meeting_relay_result_id' => row.id } },
+    ->(row) { { 'meeting_relay_result' => { 'id' => row.id } } }
+  ].each_with_index do |req, index|
+    #
+    # VALID data: EXISTING ID
+    #
+    context "with valid & solved #req data (valid @ depth #{index})," do
+      let(:fixture_row) do
+        # Choose valid domain data only (TODO: data fix is required)
+        GogglesDb::MeetingRelayResult.where("relay_code != ''")
+                                     .where('team_affiliation_id is not null')
+                                     .first(200).sample
+      end
+      let(:fixture_req) { req.call(fixture_row) }
+      let(:expected_id) { fixture_row.id }
+      subject do
+        expect(fixture_row).to be_a(GogglesDb::MeetingRelayResult).and be_valid
+        solver = Solver::Factory.for('MeetingRelayResult', fixture_req)
+        solver.solve!
+        solver
+      end
+      it_behaves_like('Solver strategy, OPTIONAL bindings, solvable req, after #solve!', GogglesDb::MeetingRelayResult)
+    end
+  end
+  #-- -------------------------------------------------------------------------
+  #++
+
+  [
+    # depth 0
+    lambda { |row|
+      {
+        'meeting_program_id' => row.meeting_program_id,
+        'team_id' => row.team_id,
+        'team_affiliation_id' => row.team_affiliation_id,
+        'relay_code' => row.relay_code,
+        'meeting_relay_result_rank' => row.rank,
+        'meeting_relay_result_reaction_time' => row.reaction_time,
+        'meeting_relay_result_minutes' => row.minutes,
+        'meeting_relay_result_seconds' => row.seconds,
+        'meeting_relay_result_hundredths' => row.hundredths,
+        'meeting_relay_result_standard_points' => row.standard_points,
+        'meeting_relay_result_meeting_points' => row.meeting_points
+      }
+    },
+    # depth 1
+    lambda { |row|
+      {
+        'meeting_relay_result' => {
+          'meeting_program_id' => row.meeting_program_id,
+          'team_id' => row.team_id,
+          'team_affiliation_id' => row.team_affiliation_id,
+          'relay_code' => row.relay_code,
+          'rank' => row.rank,
+          'reaction_time' => row.reaction_time,
+          'minutes' => row.minutes,
+          'seconds' => row.seconds,
+          'hundredths' => row.hundredths,
+          'standard_points' => row.standard_points,
+          'meeting_points' => row.meeting_points
+        }
+      }
+    },
+    lambda { |row|
+      {
+        'meeting_relay_result' => {
+          'relay_code' => row.relay_code,
+          'rank' => row.rank,
+          'reaction_time' => row.reaction_time,
+          'minutes' => row.minutes,
+          'seconds' => row.seconds,
+          'hundredths' => row.hundredths,
+          'standard_points' => row.standard_points,
+          'meeting_points' => row.meeting_points
+        },
+        'meeting_program_id' => row.meeting_program_id,
+        'team_id' => row.team_id,
+        'team_affiliation_id' => row.team_affiliation_id
+      }
+    },
+    # depth 2
+    lambda { |row|
+      {
+        'meeting_relay_result' => {
+          'relay_code' => row.relay_code,
+          'rank' => row.rank,
+          'reaction_time' => row.reaction_time,
+          'minutes' => row.minutes,
+          'seconds' => row.seconds,
+          'hundredths' => row.hundredths,
+          'standard_points' => row.standard_points,
+          'meeting_points' => row.meeting_points
+        },
+        'meeting_program' => { 'id' => row.meeting_program_id },
+        'team' => { 'id' => row.team_id },
+        'team_affiliation' => { 'id' => row.team_affiliation_id }
+      }
+    }
+  ].each_with_index do |req, index|
+    #
+    # VALID data: EXISTING row data
+    #
+    context "with solvable #req data (valid w/ layout #{index})," do
+      let(:fixture_row) do
+        # Choose valid domain data only (TODO: data fix is required)
+        GogglesDb::MeetingRelayResult.where("relay_code != ''")
+                                     .where('team_affiliation_id is not null')
+                                     .first(200).sample
+      end
+      let(:fixture_req) { req.call(fixture_row) }
+      let(:expected_id) { fixture_row.id }
+      subject do
+        expect(fixture_row).to be_a(GogglesDb::MeetingRelayResult).and be_valid
+        solver = Solver::Factory.for('MeetingRelayResult', fixture_req)
+        solver.solve!
+        solver
+      end
+      it_behaves_like('Solver strategy, OPTIONAL bindings, solvable req, after #solve!', GogglesDb::MeetingRelayResult)
+
+      describe '#entity' do
+        %i[
+          meeting_program_id team_affiliation_id team_id
+        ].each do |column_name|
+          it "has the expected #{column_name}" do
+            expect(subject.entity.send(column_name)).to eq(fixture_row.send(column_name))
+          end
+        end
+      end
+    end
+    #-- -----------------------------------------------------------------------
+    #++
+
+    #
+    # VALID data: NEW row data
+    #
+    context "with solvable NEW #req data (valid w/ layout #{index})," do
+      let(:fixture_row) do
+        badge = FactoryBot.create(:badge)
+        FactoryBot.build(
+          :meeting_relay_result,
+          team_id: badge.team_id,
+          team_affiliation_id: badge.team_affiliation_id,
+          meeting_program_id: FactoryBot.create(:meeting_program_relay).id,
+          entry_time_type_id: GogglesDb::EntryTimeType::LAST_RACE_ID
+        )
+      end
+      let(:fixture_req) { req.call(fixture_row) }
+      let(:expected_id) { false }
+      subject do
+        expect(fixture_row).to be_a(GogglesDb::MeetingRelayResult).and be_valid
+        solver = Solver::Factory.for('MeetingRelayResult', fixture_req)
+        solver.solve!
+        solver
+      end
+      it_behaves_like('Solver strategy, OPTIONAL bindings, solvable req, after #solve!', GogglesDb::MeetingRelayResult)
+
+      describe '#entity' do
+        # Check all prepared fields:
+        %i[
+          meeting_program_id team_affiliation_id team_id
+        ].each do |column_name|
+          it "has the expected #{column_name}" do
+            expect(subject.entity.send(column_name)).to eq(fixture_row.send(column_name))
+          end
+        end
+      end
+    end
+  end
+  #-- -------------------------------------------------------------------------
+  #++
 end
