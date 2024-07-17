6411aadcfc93d7c1a7e3f945d031dbb6ac0ef4e8
steveoro <steve.alloro@gmail.com>
2024-07-17 13:02:13 +0200

    improved left outer joins for caching UserResult sub-entities in MIR::TableComponent so that Bullet doesn't complain when showing UserWorkshops

--- app/components/mir/table_component.rb
+++ app/components/mir/table_component.rb
@@ -22,7 +22,7 @@ module MIR
     # - managed_team_ids: array of integer Team IDs that can be "managed" by the current user;
     #                     a +nil+ value will disable the rendering check for the action buttons.
     # - current_swimmer_id: current_user.swimmer_id value, if any.
-    def initialize(mirs:, managed_team_ids:, current_swimmer_id:)
+    def initialize(mirs:, managed_team_ids:, current_swimmer_id:) # rubocop:disable Metrics/PerceivedComplexity
       super
       @mirs = if mirs.is_a?(ActiveRecord::Relation) && mirs.first.is_a?(GogglesDb::MeetingIndividualResult)
                 mirs&.joins(:meeting, :meeting_program, :swimmer, :team, season: :season_type)
@@ -31,7 +31,9 @@ module MIR
                                laps: [:meeting_individual_result],
                                meeting_program: %i[meeting season])
               else
-                mirs
+                mirs&.joins(:user_workshop, :swimmer, season: :season_type)
+                    &.left_outer_joins(user_laps: [:user_result])
+                    &.includes(:swimmer, :season_type, user_laps: [:user_result])
               end
       @managed_team_ids = managed_team_ids
       @current_swimmer_id = current_swimmer_id
