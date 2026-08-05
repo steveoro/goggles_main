c2ddb0d216e684eccfa7b88916b3c9249f19ab71
steveoro <steve.alloro@gmail.com>
2026-08-06 00:18:15 +0200

    copied migrations; engine and bundle update

--- lib/version.rb
+++ lib/version.rb
@@ -27,10 +27,10 @@ module Version
   MINOR = '9'
 
   # Patch version.
-  PATCH = '27'
+  PATCH = '28'
 
   # Current build version.
-  BUILD = '20260803'
+  BUILD = '20260806'
 
   # Full versioning for the current release.
   FULL = "#{MAJOR}.#{MINOR}.#{PATCH} (#{CORE}-#{BUILD})".freeze
@@ -39,5 +39,5 @@ module Version
   SEMANTIC = "#{MAJOR}.#{MINOR}.#{PATCH}".freeze
 
   # Current internal DB version (independent from migrations and framework release)
-  DB = '2.10.04'
+  DB = '2.10.05'
 end
