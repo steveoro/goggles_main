dcd60f607e772850cf2b00fa334bd40ca9b24dba
steveoro <steve.alloro@gmail.com>
2026-09-21 19:57:37 +0200

    resync w/ base engine; bundle updated; added new data migrations for new seasons and categories; v. 0.10.48, db 2.10.09

--- lib/version.rb
+++ lib/version.rb
@@ -27,10 +27,10 @@ module Version
   MINOR = '10'
 
   # Patch version.
-  PATCH = '46'
+  PATCH = '48'
 
   # Current build version.
-  BUILD = '20260914'
+  BUILD = '20260921'
 
   # Full versioning for the current release.
   FULL = "#{MAJOR}.#{MINOR}.#{PATCH} (#{CORE}-#{BUILD})".freeze
@@ -39,5 +39,5 @@ module Version
   SEMANTIC = "#{MAJOR}.#{MINOR}.#{PATCH}".freeze
 
   # Current internal DB version (independent from migrations and framework release)
-  DB = '2.10.08'
+  DB = '2.10.09'
 end
